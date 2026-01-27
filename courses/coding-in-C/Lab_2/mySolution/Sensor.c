d026718d9f1b04625653c51e00967894fa19ddc1
Brown01 <Brown01@email.com>
2026-01-27 18:42:11 +0100

    create bit array with number

--- courses/coding-in-C/Lab_2/mySolution/Sensor.c
+++ courses/coding-in-C/Lab_2/mySolution/Sensor.c
@@ -1,8 +1,23 @@
-#include ><stdio.h>
+#include <stdio.h>
 
 unsigned char status = 0xA7;
+int code[7] = {0};
 
 int main() {
     
+    for (int i = 7; i >= 0; i--) {
+        int bit = (status >> i) & 1;
+        printf("Bit %d: %d\n", i, bit);
+    }
+        
+    for (int i = 7; i >= 0; i--) {
+        code[i] = (status >> i) &1;
+    
+    }
+    for (int i = 0; i <= 7; i++) {
+        printf("%d", code[i]);
+    
+    }
+
     
 }
