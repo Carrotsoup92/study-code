964e4f963766a873cf0448834efaf113ac8364cf
Brown01 <Brown01@email.com>
2026-01-28 11:25:46 +0100

    add error cases

--- courses/coding-in-C/Lab_2/mySolution/Sensor.c
+++ courses/coding-in-C/Lab_2/mySolution/Sensor.c
@@ -2,22 +2,36 @@
 
 unsigned char status = 0xA7;
 int code[7] = {0};
+int test, test1 = 0;
 
 int main() {
     
-    for (int i = 7; i >= 0; i--) {
-        int bit = (status >> i) & 1;
-        printf("Bit %d: %d\n", i, bit);
+    if (test == 1){
+        for (int i = 7; i >= 0; i--) {
+            int bit = (status >> i) & 1;
+            printf("Bit %d: %d\n", i, bit);
+        }
     }
-        
+         
     for (int i = 7; i >= 0; i--) {
-        code[i] = (status >> i) &1;
-    
-    }
-    for (int i = 0; i <= 7; i++) {
-        printf("%d", code[i]);
-    
+        code[i] = (status >> i) & 1;
     }
 
+    if (test1 == 1){
+        for (int i = 0; i <= 7; i++) {
+        printf("%d", code[i]);
+        }
+    printf("\n");
+    }
     
+    if (code[7] == 1){
+        printf("There in an error.\n");
+    } 
+    if (code[6] == 1){
+        printf("There in an overheating.\n");
+    } 
+    if (code[5] == 1){
+        printf("There in an undervoltage.\n");
+    }
+       
 }
