ec12793679d91383c57513f3b8bbd4c349107081
Blue-01 <Blue-01@mail.com>
2026-01-30 19:27:55 +0100

    add chessfield soluton

--- courses/coding-in-C/Lab_2/mySolution/Chessfield2.c
+++ courses/coding-in-C/Lab_2/mySolution/Chessfield2.c
@@ -7,28 +7,44 @@ char field_name [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
 int main(){
 
-    printf("    ");
-    for (int i = 0; i < 8; i++){
+    printf("     ");
+    for (int i = 0; i < field_size; i++){
         printf("%c", field_name[i]);
         printf("   ");
     } 
     printf("\n");
-  
-    printf("  +");
-    for (int i = 0; i < 8; i++){
+    printf("   +");
+    
+    for (int i = 0; i < field_size; i++){
         printf("---+");
     }
     printf("\n");
 
-    for (int i = 8; i > 0; i--){
+    for (int i = field_size; i > 0; i--){
+
         printf("%d ", i);
-        printf("|");
+        printf(" |");
+
+        if(i %2 == 0){
             for (int j = 0; j < 4; j++){
-                printf("###|   |");
+            printf("###|   |");
             }
-            printf("\n");
-            for (int k = 0; k < 8; k++){
-                printf("---+");
+        } else {
+            for (int j = 0; j < 4; j++){
+            printf("   |###|");
             }
+        }
+        printf("\n");
+        printf("   +");
+
+        for (int j = 0; j < field_size; j++){
+            printf("---+");
+        }
+        printf("\n");
     }
+    printf("     ");
+    for (int i = 0; i < field_size; i++){
+        printf("%c", field_name[i]);
+        printf("   ");
+    } 
 }
