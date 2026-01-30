6540cddfbba2c0151d1aa429a81b02b7884312e4
Brown01 <Brown01@email.com>
2026-01-30 18:54:59 +0100

    add early field strukture

--- courses/coding-in-C/Lab_2/mySolution/Chessfield2.c
+++ courses/coding-in-C/Lab_2/mySolution/Chessfield2.c
@@ -6,7 +6,29 @@ int field_size = 8;
 char field_name [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
 int main(){
-    for (int i = 1; i < 8; i++){
+
+    printf("    ");
+    for (int i = 0; i < 8; i++){
         printf("%c", field_name[i]);
+        printf("   ");
+    } 
+    printf("\n");
+  
+    printf("  +");
+    for (int i = 0; i < 8; i++){
+        printf("---+");
+    }
+    printf("\n");
+
+    for (int i = 8; i > 0; i--){
+        printf("%d ", i);
+        printf("|");
+            for (int j = 0; j < 4; j++){
+                printf("###|   |");
+            }
+            printf("\n");
+            for (int k = 0; k < 8; k++){
+                printf("---+");
+            }
     }
 }
