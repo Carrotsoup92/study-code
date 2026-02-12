b77c6bc44d57951a37356377b8613ab872700028
Brown01 <Brown01@email.com>
2026-02-12 15:56:11 +0100

    add new files

--- courses/coding-in-C/Lab_4/mySolution/palindrome.c
+++ courses/coding-in-C/Lab_4/mySolution/palindrome.c
@@ -2,7 +2,18 @@
 // This program checks if a given string is a palindrome or not.
 
 #include <stdio.h>
+#include <string.h>
+#include "palin.h"
 
 int main() {
+    char word[100];
+    char palindrome[100];
 
+    printf("Please enter your word (max 100 characters):");
+    fgets(word, sizeof(word), stdin);
+    int lenght = sizeof(word);
+
+
+    printf("%s", word);
+    printf("%d", lenght);
 }
