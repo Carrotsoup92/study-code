71806e53679e9ee707c9b00639dd1bfdb10a89a1
Carrot <Carrot@email.com>
2026-02-13 20:36:47 +0100

    add sim_particle.c solution

--- courses/coding-in-C/Lab_5/mySolution/sim_particle.c
+++ courses/coding-in-C/Lab_5/mySolution/sim_particle.c
@@ -12,18 +12,19 @@ int main() {
     
     enum {LENGTH = 10};
     int sim_array [LENGTH] = {0};
+    int temp_array [LENGTH] = {0};
+    int *p_sim_array = sim_array;
+    int *p_temp_array = temp_array;
 
-    int particle = 2; 
-    for (int i = 0; i < 3; i++){    //set particle(1) by index 2,4,6
+    //set particle(1) by index 2,4,6
+    for (int i = 0; i < 3; i++){  
+        int particle = 2;  
         sim_array[particle] = 1;
         particle += 2;
     }
 
-    int *p_sim_array = sim_array;
-    int temp_array [LENGTH] = {0};
-    int *p_temp_array = temp_array;
-
-    for (int i = 0; i < LENGTH; i++) {  //clone array
+    //clone array
+    for (int i = 0; i < LENGTH; i++) {  
         p_temp_array[i] = p_sim_array[i];
     }
 
@@ -41,11 +42,27 @@ int main() {
         for (int j= 0; j < LENGTH; j++) {
 
             if (*(p_temp_array + j) == 1){
-                random = rand() % 2;        // creare random number 0 or 1
+                random = rand() % 2;        // create random number 0 or 1
                 random = random * 2 - 1;    // change in -1 or 1
                 *(p_sim_array + j) = 0;
+                
+                // printf("%d\n", random);
 
-                if (*(p_sim_array + j + random) == 1) {  //check collision
+                // set border
+                switch (j)  
+                {
+                case 0:
+                    random = 1;
+                    break;
+                case LENGTH - 1:
+                    random = -1;
+                    break;
+                default:
+                    break;
+                }
+
+                //check collision
+                if (*(p_sim_array + j + random) == 1) {  
                     *(p_sim_array + j + random) = 0;
                     printf("Collision on index %d\n\n", j + random);
                 } else {
@@ -58,17 +75,5 @@ int main() {
             p_temp_array[j] = p_sim_array[j];
         }
     }
-
-
-
-
-
-
-    // for (int i = 0; i < LENGTH; i++) {
-    //     printf("%d ", sim_array[i]);
-    // }
-    // printf("\n");
-    // for (int i = 0; i < LENGTH; i++) {
-    //     printf("%d ", temp_array[i]);
-    // }
+    return 0;
 }
