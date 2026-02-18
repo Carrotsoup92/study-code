9ebd4c00b93f9c9109a606d15f04d682cbbd4837
Carrot <Carrot@email.com>
2026-02-18 21:25:19 +0100

    fix decimal place by float number

--- courses/coding-in-C/Lab_6/mySolution/section_2.c
+++ courses/coding-in-C/Lab_6/mySolution/section_2.c
@@ -16,7 +16,7 @@ struct car
 void refuel(float fuel, struct car *c) {
     if(fuel + c->fuel_level < c->max_fuel_level) {
         c->fuel_level =+ c->fuel_level + fuel;
-        printf("%s fuel level: %f\n", c->model, c->fuel_level);
+        printf("%s fuel level: %.2f\n", c->model, c->fuel_level);
     } else {
         printf("cannot refuel %s\n", c->model);
     }
@@ -33,21 +33,19 @@ int main() {
     c1.max_fuel_level = 100;
     strcpy(c1.model, "Golf 7");
 
-    printf("Car1 fuel level: %2f\n", c1.fuel_level);
-    printf("Car1 max fuel level: %2f\n", c1.max_fuel_level);
+    printf("Car1 fuel level: %.2f\n", c1.fuel_level);
+    printf("Car1 max fuel level: %.2f\n", c1.max_fuel_level);
     printf("Car1 1model: %s\n", c1.model);
     printf("\n");
 
-    printf("Car2 fuel level: %2f\n", c2.fuel_level);
-    printf("Car2 max fuel level: %2f\n", c2.max_fuel_level);
+    printf("Car2 fuel level: %.2f\n", c2.fuel_level);
+    printf("Car2 max fuel level: %.2f\n", c2.max_fuel_level);
     printf("Car2 1model: %s\n", c2.model);
 
     float fuel = 90;
 
     refuel(fuel, &c1);
     refuel(fuel, &c2);
-
-    
 
     return  0;
 }
