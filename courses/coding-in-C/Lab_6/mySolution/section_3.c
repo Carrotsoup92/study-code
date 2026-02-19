da6283dd655eded84f9408b650ce9394bc87fc06
Carrot <Carrot@email.com>
2026-02-19 20:35:13 +0100

    add section III solution

--- courses/coding-in-C/Lab_6/mySolution/section_3.c
+++ courses/coding-in-C/Lab_6/mySolution/section_3.c
@@ -25,10 +25,68 @@
     if (file == NULL) {
         printf("Error opening file!\n");
     } else {
-        printf("File is open\n");
+        // printf("File is open\n");
+    }
+ }
+
+ void set_object_detection(int length, struct sensor *s) {
+    for (int i = 0; i < length; i++) {
+        if(s->data[i].porbability > s->threshold) {
+            s->object_detection[i] = 1;
+        }
     }
  }
  
+void time_intervals(int length, struct sensor *s){
+    printf("Sensor %d detections:\n", s->id);
+
+    int active = 0;// Detection state: 0 = no object detected, 1 = object detected
+    for (int i = 0; i < length; i++){
+
+        // Detect start of an interval (transition from 0 → 1)
+        if (s->object_detection[i] == 1 && active == 0){
+            printf("Start: %.2f s ", s->data[i + 1].time);
+            active = 1;
+        }
+        // Detect end of an interval (transition from 1 → 0)
+        if (s->object_detection[i] == 0 && active == 1)  {
+            printf("End: %.2f s\n", s->data[i - 1].time);
+            active = 0;
+        }        
+    }
+    // If detection remains active at the final time step,
+    // close the interval using the last timestamp
+    if (active == 1) {
+            printf("End: %.2f s\n", s->data[length - 1].time);
+        }
+    printf("\n");
+}
+
+void combined_signal(int length, struct sensor *s1, struct sensor *s2){
+    int active = 0;
+    
+    for (int i = 0; i < length; i++) {
+        if (s1->object_detection[i] ==1  && s2->object_detection[i] == 1){
+            if (active == 0) {
+                printf("Start: %.2f s ", s1->data[i].time);
+                active = 1;
+            }
+        } else {
+            if (active == 1 ) {
+                printf("End: %.2f s ", s1->data[i - 1].time);
+                active = 0;
+            }
+        }
+    }
+    // If still active at end
+    if (active == 1)
+    {
+        printf("End: %.2f s\n", s1->data[length - 1].time);
+    }
+
+    printf("\n");
+}
+
 int main() {
 
     FILE *file1 = fopen("C:/Meine_Projekte/VS_Code/study-code/courses/coding-in-C/Lab_6/sensor1.txt", "r"); // r to read only
@@ -39,7 +97,7 @@ int main() {
     struct sensor s1 = {.id = 1, .threshold = 0.8};
     struct sensor s2 = {.id = 2, .threshold = 0.7};
 
-    int length = 760;
+    int length = 3000;
 
     for (int i = 0; i < length; i++){
         fscanf(file1, "%f %lf", &s1.data[i].time, &s1.data[i].porbability);
@@ -47,31 +105,13 @@ int main() {
     }
     fclose(file1);
 
-    // for (int i = 0; i < length; i++){
-    //     printf("%.2f, %.3lf ", s1.data[i].time, s1.data[i].porbability);
-    //     printf("%.2f, %.3lf\n", s2.data[i].time, s2.data[i].porbability);
-    // }
+    set_object_detection(length, &s1);
+    set_object_detection(length, &s2);
 
-    for (int i = 0; i < length; i++) {
-        if(s1.data[i].porbability > s1.threshold) {
-            s1.object_detection[i] = 1;
-        }
-        if(s2.data[i].porbability > s2.threshold) {
-            s2.object_detection[i] = 1;
-        }
-    }
+    time_intervals(length, &s1);
+    time_intervals(length, &s2);
 
-    // for (int i = 0; i < length; i++) {
-    //     printf("%d ", s1.object_detection[i]);
-    //     printf("%d\n", s2.object_detection[i]);
-    // }
-
-    for (int i = 0; i < length; i++){
-        if(s1.object_detection[i] < s1.object_detection[i + 1]){
-            printf("Start: %.2f s ", s1.data[i +1].time);
-        }
-
-    }
+    combined_signal(length, &s1, &s2);
 
     return 0;
 }
