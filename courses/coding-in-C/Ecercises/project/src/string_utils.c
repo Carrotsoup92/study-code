90ad1437c4e406b97022d27beebb6aff85ce7698
Carrotsoup92 <carrotsoup@web.de>
2026-02-25 16:26:44 +0000

    Exercises

---
+++ courses/coding-in-C/Ecercises/project/src/string_utils.c
@@ -0,0 +1,18 @@
+/*
+ * File: string_utils.c
+ * Description: 
+ */
+
+//  #include"string_utils.h"
+ #include <stdio.h>
+
+ int main() {
+
+    int length;
+
+    scanf("%d", &length);
+
+    printf("%d\n", length);
+    return 0;
+
+ }
