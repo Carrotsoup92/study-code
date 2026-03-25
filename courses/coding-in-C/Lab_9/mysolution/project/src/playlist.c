627269d9426171b4b09529483ef7c5e0cb205569
carrot <carrot@email.com>
2026-03-25 22:13:53 +0100

    add playlist functions

--- courses/coding-in-C/Lab_9/mysolution/project/src/playlist.c
+++ courses/coding-in-C/Lab_9/mysolution/project/src/playlist.c
@@ -17,11 +17,12 @@
  * @param[out] param2 Explain parameter 2 (Output/Pointer)
  * @return            Explanation of return value
  */
-Playlist * init_playlist(void) {
-
+Playlist * init_playlist(void) 
+{
     Playlist *p_list = malloc(sizeof(Playlist));
 
-    if (p_list == NULL) {
+    if (p_list == NULL) 
+    {
         printf("Error by init Playlist!");
         return NULL;
     }
@@ -32,11 +33,12 @@ Playlist * init_playlist(void) {
     return p_list;
 }
 
-void add_song(Playlist *p_list, char *title, char *artist ) {
-
+void add_song(Playlist *p_list, char *title, char *artist ) 
+{
     Song *p_newsong = malloc(sizeof(Song));
 
-    if (p_newsong == NULL) {
+    if (p_newsong == NULL) 
+    {
         printf("Error by add song!");
         return;
     }
@@ -44,7 +46,8 @@ void add_song(Playlist *p_list, char *title, char *artist ) {
     p_newsong->artist = malloc(strlen(title) + 1);
     p_newsong->title = malloc(strlen(artist) + 1);
 
-    if (p_newsong->artist == NULL || p_newsong->title == NULL) {
+    if (p_newsong->artist == NULL || p_newsong->title == NULL) 
+    {
         printf("Error by add artist or title");
         free(p_newsong->artist);
         free(p_newsong->title);
@@ -56,23 +59,52 @@ void add_song(Playlist *p_list, char *title, char *artist ) {
     strcpy(p_newsong->title, title);
     p_newsong->next = NULL;
 
-    if (p_list->head == NULL ) {
+    if (p_list->head == NULL ) 
+    {
         p_list->head = p_newsong;
         p_list->tail = p_newsong;
-    } else {
+    } else 
+    {
         p_list->tail->next = p_newsong;
         p_list->tail = p_newsong;
     }
 }
 
-void print_song(Playlist *p_list) {
+void print_playlist(Playlist *p_list) 
+{
+    Song *p_temp = p_list->head; 
+    printf("Title:      |Artist:");
 
+    while (p_temp->next != NULL) 
+    {
+        printf("%c|%c",p_list->head->title, p_list->head->artist);
+        p_temp->next = p_temp->next->next;        
+    }
+    p_temp = NULL;
 }
 
-void delete_song(Playlist *p_list) {
+void delete_song(Playlist *p_list) 
+{
+    Song *p_temp = p_list->head;
 
+    p_list->head = p_list->head->next;
+
+    if (p_list->head == NULL)
+    {
+        p_list->tail = NULL;
+    }
+
+    free(p_temp);
+    p_temp = NULL;
 }
 
-void delete_playlist(Playlist *p_list) {
-
+void delete_playlist(Playlist *p_list) 
+{
+    if (p_list->head == NULL)
+    {
+        return;
+    }
+    delete_song(p_list);
+    free(p_list);
+    p_list = NULL;
 }
