845519eca80b3a4a2ad666b15c369432a8297523
Carrotsoup92 <carrotsoup@web.de>
2026-04-13 14:28:36 +0000

    add readstudentdata function

--- courses/coding-in-C++/Lab_1/mysolution/input_data.cpp
+++ courses/coding-in-C++/Lab_1/mysolution/input_data.cpp
@@ -10,7 +10,39 @@
 #include "input_data.hpp"
 
 
-void read_student_data()
+void read_student_data(std::string& name, double& homework, double& midterm, double& finalExam)
 {
+    std::cout << "Please enter student name: "; 
+    while (true)
+    {
+        std::getline(std::cin, name);
+        if (name.empty())
+        {
+            std::cout << "Invalid name! Please try again!\n";
+        }
+        else
+        {
+            break;
+        }
+    }
 
+    std::cout << "Please enter homework, midterm and final exam grads: ";
+    while (true)
+    {
+        if (!(std::cin >>homework >>midterm >>finalExam))
+        {
+            std::cout << "Please enter a number\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else if (homework <0|| homework >100 || midterm <0 || midterm >100 || finalExam <0 || finalExam >100)
+        {
+            std::cout << "Please enter a valid number\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else
+        {
+            break;
+        }
+    }
 }
