530df2ea76730ac7657054bde9c1fb71babe3162
Carrot <Carrot@email.com>
2026-04-29 21:32:48 +0200

    add new files

---
+++ courses/coding-in-C++/Lab_2/mySolution/section_7/include/query.hpp
@@ -0,0 +1,19 @@
+/*
+* File: query.hpp
+* Description: Shot discription of the file.
+*/
+
+#ifndef QUERY_H
+#define QUERY_H
+
+#include <iostream>
+
+class Query {
+private:
+   int placeholder;
+public:
+    Query();  // Constructor
+    ~Query(); // Destructor
+};
+
+#endif
---
+++ courses/coding-in-C++/Lab_2/mySolution/section_7/src/query.cpp
@@ -0,0 +1,17 @@
+/*
+* File: query.cpp
+* Description: Shot discription of the file.
+*/
+
+#include <iostream>
+
+/**
+ * @brief Calculates the average temperature.
+ *
+ * Returns the arithmetic mean of all valid sensor values.
+ * Invalid sensor values are ignored.
+ *
+ * @param[in] values       Array of sensor values
+ * @param[in,out] value_count  Number of elements in values
+ * @return                 Average of valid values
+ */
