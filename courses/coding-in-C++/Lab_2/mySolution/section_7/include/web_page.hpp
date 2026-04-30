1926b0c4306e17bd855795d14b5e41914876c879
carrot <carrot@email.com>
2026-04-30 14:22:15 +0200

    little change

--- courses/coding-in-C++/Lab_2/mySolution/section_7/include/web_page.hpp
+++ courses/coding-in-C++/Lab_2/mySolution/section_7/include/web_page.hpp
@@ -20,7 +20,7 @@ private:
 public:
     WebPage():name(""),content(""), ranking(0) // Constructor
     {
-        // count_web_pages++;
+        count_web_pages++;
         // std::cout << "Constructor\n" << count_web_pages << "\n";  
     }
     ~WebPage() = default; // Destructor
