34d44beae6be0a4c20fb12d88dcfbfa916de53f1
carrot <carrot@email.com>
2026-05-05 17:25:01 +0200

    continue section_1

--- courses/coding-in-C++/Lab_4/my_solution/section_1.cpp
+++ courses/coding-in-C++/Lab_4/my_solution/section_1.cpp
@@ -40,15 +40,38 @@ public:
         double length = get_length();
         return std::round(length * factor) / factor;
     }
+
+    void operator+=(const Vector2D& vec_2)
+    {
+        this->x = this->get_x() + vec_2.get_x();
+        this->y = this->get_y() + vec_2.get_y();
+    }
+
+    Vector2D operator*(double multiplicant)
+    {
+        return Vector2D(this->get_x() * multiplicant, this->get_y() * multiplicant);
+    }
 };
 
-Vector2D = ;
+Vector2D operator+(const Vector2D& vec_1, const Vector2D& vec_2)
+{
+    return Vector2D(vec_1.get_x() + vec_2.get_x(), vec_1.get_y() + vec_2.get_y());
+};
+
+Vector2D operator*(double multiplicant, const Vector2D& vec_2)
+{
+    return Vector2D(multiplicant * vec_2.get_x(), multiplicant * vec_2.get_y());
+}
+
+void vector_equal(const Vector2D& vec_1, const Vector2D& vec_2)
+{
+    
+}
+
 int main()
 {
     Vector2D vector(4, 4);
 
-    vector.get_x();
-    vector.get_y();
     vector.print_vec();
 
     double length = vector.get_length();
@@ -56,4 +79,18 @@ int main()
 
     double length_1 = vector.get_length(3);
     std::cout << "length: " << length_1 << "\n";
+
+    Vector2D vector_2(5, 5);
+
+    Vector2D vector_3 = vector + vector_2;
+    vector_3.print_vec();
+
+    vector_3 = vector_3 * 4;
+    vector_3.print_vec();
+
+    vector += vector_2;
+    vector.print_vec();
+
+    vector_2 = 2.5 * vector_2;
+    vector_2.print_vec();
 }
