165592ac090a6b8639b387b7b9810cac736f4c26
carrot <carrot@email.com>
2026-05-18 12:57:50 +0200

    improve weightCheck class

--- courses/coding-in-C++/Lab_4/my_solution/section_4/inspection.hpp
+++ courses/coding-in-C++/Lab_4/my_solution/section_4/inspection.hpp
@@ -15,23 +15,39 @@ private:
     std::string name;
 protected:
     int placeholder;
+    void set_name(std::string name)
+    {
+        this->name = name;
+    }
 public:
     Inspection();  // Constructor
-    ~Inspection() = default; // Destructor
+    virtual~Inspection() = default; // Destructor
+
+    virtual void print_report() const = 0;
+    std::string get_name() const
+    {
+        return this->name;
+    }
 };
 
 
-class WeightCheck
+class WeightCheck : public Inspection
 {
 private:
-    /* data */
+    int max_weight;
+    int min_weight;
 public:
-    WeightCheck(/* args */);
-    ~WeightCheck();
+    WeightCheck(int max_weight, int min_weight): max_weight(max_weight), min_weight(min_weight) 
+    {
+        set_name("Weight_check");
+    };
+    ~WeightCheck() = default;
+
+    void print_report() const override;
 };
 
 
-class VisualInspection
+class VisualInspection :public Inspection
 {
 private:
     /* data */
@@ -40,7 +56,7 @@ public:
     ~VisualInspection();
 };
 
-class TemperatureTest
+class TemperatureTest :public Inspection
 {
 private:
     /* data */
