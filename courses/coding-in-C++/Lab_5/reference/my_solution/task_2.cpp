f6ec373da2f22e5c49d73d5bb82e30b0c5d4b121
Carrot <Carrot@email.com>
2026-06-01 19:50:09 +0200

    add lab5

--- courses/coding-in-C++/Lab_5/reference/my_solution/task_2.cpp
+++ courses/coding-in-C++/Lab_5/reference/my_solution/task_2.cpp
@@ -4,14 +4,27 @@
 */
 
 #include <iostream>
-#include <string>
 
 template <typename T, int N>
+void print(T (&array)[N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << array[i] << "; ";
+    }
+    std::cout << "\n";
+}
 
 
-int main() {
-    
-    void print(T, (&array_1)[5]);
+int main() 
+{
+    int array_1[] = {2,3,5,9,8,4};
+    double array_2[] = {2.3, 3.0, 5.6, 5.8};
+    char array_3[] = {'v', 'B', 'O', 'e'};
+
+    print(array_1);
+    print(array_2);
+    print(array_3);
     
     return 0;
 }
---
+++ courses/coding-in-C++/Lab_5/reference/my_solution/task_4.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file tast_4.cpp
+ * @brief Weakest signal detector
+ */
+
+#include <iostream>
+#include <string>
+
+template <typename T, typename N>
+void print_pair(std::string label_1, T, std::string Label_2, N)
+{
+      4 
+}
+
+
+
+
+int main()
+{
+    int array_1[] = {5,6,8,4,9,2,3};
+    double array_2[] = {1.0, 5.6, 7.8, 0.6};
+
+    print(array_1);
+    print(array_2);
+}
