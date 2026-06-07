2b1bfb792d0e96dd11d57ead2fb046f878cdc75b
Carrot <Carrot@email.com>
2026-06-07 19:37:04 +0200

    finish lab6

--- courses/coding-in-C++/Lab_5/reference/my_solution/section_3.cpp
+++ courses/coding-in-C++/Lab_5/reference/my_solution/section_3.cpp
@@ -16,6 +16,20 @@ T sum_of_all_values(std::vector<T>& vec)
     return sum;
 }
 
+template <>
+bool sum_of_all_values<bool>(std::vector<bool>& vec)
+{
+    int count_true = 0;
+    int count_false = 0;
+
+    for( bool temp : vec)
+    {
+        (temp == true) ? count_true++ : count_false++;
+    }
+
+    return (count_true >= count_false);
+}
+
 template <typename T>
 T largest_value(std::vector<T>& vec)
 {
@@ -25,7 +39,7 @@ T largest_value(std::vector<T>& vec)
 }
 
 template <typename T>
-double averge_value(std::vector<T>& vec)
+double average_value(std::vector<T>& vec)
 {
     T sum = sum_of_all_values(vec);
     int size = vec.size();
@@ -47,7 +61,7 @@ void analyzer(std::vector<T>& vec)
 {
     auto sum = sum_of_all_values(vec);
     auto largest = largest_value(vec);
-    auto average = averge_value(vec);
+    auto average = average_value(vec);
 
     std::cout << "Data Report:\n";
     std::cout << "Raw data: ";
@@ -56,6 +70,12 @@ void analyzer(std::vector<T>& vec)
     std::cout << "Sum of all values: " << sum << "\n";
     std::cout << "Maximun value: " << largest << "\n";
     std::cout << "Average value: " << average << "\n";
+}
+
+template <>
+void analyzer<bool>(std::vector<bool>& vec)
+{
+    std::cout << "Motor was active for most of the test run: " << std::boolalpha << sum_of_all_values(vec) << "\n";
 }
 
 template <typename T, int N>
@@ -72,6 +92,8 @@ int main()
 {
     std::vector<int> vec = {42, 17, 42, 5, 99, 17, 63, 12, 0};
     std::vector<double> vec_d = {3.5, 1.25, 7.0, 2.75, 9.9, 0.5};
+    std::vector<bool> vec_b = {1,1,1,1,0,0,1,1,0,0};
+    std::vector<bool> vec_b_2 = {0,0,0,0,0,0,0,0,1,1};
 
     int data[] = {42, 17, 42, 5, 99, 17, 63, 12, 0, 7, 9, 2};
     double data_d[] = {3.5, 1.25, 7.0, 2.75, 9.9, 0.5, 5.6, 0.3, 9.2};
@@ -83,6 +105,9 @@ int main()
     analyzer(vec_d);
     analyzer(vec_i_2);
     analyzer(vec_d_2);
+
+    analyzer(vec_b);
+    analyzer(vec_b_2);
 
     return 0;
 }
